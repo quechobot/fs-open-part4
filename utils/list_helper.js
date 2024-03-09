ecd0dba6ea559e0f5424e2d210bfaf96df2ee61c
cristopher Quecholac <alum.2c@gmail.com>
2024-03-08 18:02:09 -0600

    create functions mostBLogs mostLikes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+const {maxBy} = require("lodash/math");
 const dummy = (blogs) => {
     return 1
 }
@@ -7,7 +9,39 @@ const totalLikes = (blogs) => {
       : 0;
   return likes
 }
+
+const mostBlogs = (blogs) => {
+    const authors = (_.uniqBy(blogs, 'author')).map( blog => blog.author)
+    const blogsByAuthor = _.countBy(blogs,'author')
+    let maxBlogger = {author:undefined, blogs:0};
+    authors.forEach( author => {
+        let blogsByAuthor = blogs.filter(blog=>{
+            return blog.author === author
+        }).length
+        if(blogsByAuthor > maxBlogger.blogs ){
+            maxBlogger= {author: author, blogs: blogsByAuthor}
+        }
+    })
+    return maxBlogger
+}
+
+const mostLikes = (blogs) => {
+    const authors = (_.uniqBy(blogs, 'author')).map( blog => blog.author)
+    let mostLiked = {author:undefined, likes:0}
+    authors.forEach( author => {
+        let blogsByAuthor = blogs.filter(blog=>{
+            return blog.author === author
+        })
+        let likes = _.sumBy(blogsByAuthor, "likes")
+        if(likes > mostLiked.likes ){
+            mostLiked = {author: author, likes: likes}
+        }
+    })
+    return mostLiked
+}
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    mostLikes,
+    mostBlogs
 }
