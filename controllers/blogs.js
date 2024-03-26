a48b4efcb43be28ffa7433eaad72fcbc6e53737e
Cristopher <alum.2c@gmail.com>
2024-03-26 14:54:40 -0600

    exercises  4.18 - 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,16 +2,29 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const {update} = require("lodash/object");
+const jwt = require('jsonwebtoken')
+
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find().populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
     if (body.title === undefined || body.url === undefined){
         response.status(400).end()
     }
-    const user = await User.findById(body.user)
+    const user = await User.findById(decodedToken.id)
     const blog = new Blog({
         title: body.title,
         author: body.author,
